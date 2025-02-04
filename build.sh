3383c116140d9a64cb8854af310130e177ef21ce
ඞrcher <archer.h61m@gmail.com>
2025-02-04 15:02:37 +0700

    bring up susfs to official repo link

--- build.sh
+++ build.sh
@@ -43,7 +43,7 @@ fi
 if [ "$KernelSU" = true ]; then
     if [ "$SUSFS4KSU" = true ]; then
         echo "SUSFS4KSU is enabled. Cloning GalaxyBuild KernelSU-Next..."
-        curl -sL https://raw.githubusercontent.com/galaxybuild-project/tools/refs/heads/main/Scripts/KernelSU-SuSFS.sh | bash -s next
+        curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
         ensure_config arch/arm64/configs/$KERNEL_DEFCONFIG CONFIG_KSU CONFIG_KSU_SUSFS CONFIG_KSU_SUSFS_SUS_SU KSU_SUSFS_HAS_MAGIC_MOUNT
     else
         if [ ! -d "KernelSU" ]; then
